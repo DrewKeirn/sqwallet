6a0cc012d332dfa0b176a4dd0741b848e74da7cf
Andrew Cristopher Keirn <keirna@purdue.edu>
2020-06-09 11:11:53 -0700

    brought back navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router/*, Route*/ } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
 
 // routes to components
-// import Navbar from "./components/navbar.component";
+import Navbar from "./components/navbar.component";
 
 function App() {
   return (
     <Router>
       <div className="container">
-      {/* <Navbar /> */}
-      <br />
+        <Navbar />
+        <br/>
       </div>
     </Router>
   );
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,32 +1,31 @@
-// import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-// export default class Navbar extends Component {
-//   render() {
-//     return (
-      // <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      //   <a class="navbar-brand" href="#">Sqwallet</a>
-      //   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      //     <span class="navbar-toggler-icon"></span>
-      //   </button>
-      //   <div class="collapse navbar-collapse" id="navbarNav">
-      //     <ul class="navbar-nav">
-      //       <li class="nav-item active">
-      //         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      //       </li>
-      //       <li class="nav-item">
-      //         <a class="nav-link" href="#">Budget</a>
-      //     </li>
-      //     <li class="nav-item">
-      //       <a class="nav-link" href="#">Mortgage Calculator</a>
-      //     </li>
-      //     <li class="nav-item">
-      //       <a class="nav-link" href="#">Compound Interest Calculator</a>
-      //     </li>
-      //   </ul>
-      // </div>
-      // </nav>
-//       null
-//     );
-//   }
-// }
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Sqwallet</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Budget</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Mortgage Calculator</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Compound Interest Calculator</a>
+          </li>
+        </ul>
+      </div>
+      </nav>
+    );
+  }
+}
