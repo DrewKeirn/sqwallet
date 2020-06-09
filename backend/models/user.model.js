71eb0d63934e85d99d894db9c1b14b0bb7244460
Andrew Cristopher Keirn <keirna@purdue.edu>
2020-06-09 09:58:03 -0700

    added user schema

---
+++ backend/models/user.model.js
@@ -0,0 +1,13 @@
+import { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+}, {
+  timestamps: true,
+});
