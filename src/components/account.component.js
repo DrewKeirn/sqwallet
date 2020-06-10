66b3f08482e25b4bab9811bbba4db679cfca09c3
Andrew Cristopher Keirn <keirna@purdue.edu>
2020-06-10 14:47:10 -0700

    reverted

--- src/components/account.component.js
+++ src/components/account.component.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './signin/signin.html'
 
 export default class Account extends Component {
   render() {
     return(
       <div>
-        <p>This is the Account Component</p>
+        <p>This is the Account component</p>
       </div>
     )
   }
